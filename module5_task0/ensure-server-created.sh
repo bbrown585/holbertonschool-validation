aae9c2f55e09ee9589f85f2fa3797e79fa90e7ab
Brandon <brandon_scott585@yahoo.com>
2023-03-06 16:12:52 -0600

    test scripts

--- module5_task0/ensure-server-created.sh
+++ module5_task0/ensure-server-created.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+# Get the list of instances with a "running" status
+instances=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].{Name:PublicDnsName}" --output=text)
+
+# Check if there is at least one running instance
+if [[ -n "$instances" ]]; then
+    # If there is, print the public DNS name of the first instance found
+    echo "$instances" | head -n 1
+else
+    # If there is none, create a new instance
+    instance=$(aws ec2 run-instances --image-id ami-0c94855ba95c71c99 --count 1 --instance-type t2.micro --key-name mykey --security-group-ids sg-xxxxxxxx --subnet-id subnet-xxxxxxxx --associate-public-ip-address --query "Instances[0].{Name:PublicDnsName}" --output=text)
+
+    # Print the public DNS name of the newly created instance
+    echo "$instance"
+fi
--- module5_task0/ensure-server-setup.sh
+++ module5_task0/ensure-server-setup.sh
@@ -1,1 +1,39 @@
 #!/bin/bash
+
+# Check if an argument was passed
+if [[ $# -eq 0 ]]; then
+    echo "Error: Remote server address not provided."
+    exit 1
+fi
+
+# Get the remote server address
+server=$1
+
+# Check if SSH connection is possible
+if ! ssh -o "BatchMode yes" ubuntu@"$server" exit >/dev/null 2>&1; then
+    echo "Error: Unable to connect to remote server with SSH."
+    exit 1
+fi
+
+# Install Docker if it is not installed
+if ! ssh ubuntu@"$server" "which docker" >/dev/null 2>&1; then
+    ssh ubuntu@"$server" "sudo apt-get update && sudo apt-get install -y docker.io"
+fi
+
+# Check Docker version
+docker_version=$(ssh ubuntu@"$server" "docker -v | cut -d ' ' -f 3")
+if [[ "$docker_version" != "20.10"* ]]; then
+    ssh ubuntu@"$server" "sudo apt-get update && sudo apt-get install -y docker.io"
+fi
+
+# Update all packages
+ssh ubuntu@"$server" "sudo apt-get update && sudo apt-get upgrade -y"
+
+# Add user ubuntu to the docker group
+ssh ubuntu@"$server" "sudo usermod -aG docker ubuntu"
+
+# Test if the user can execute docker commands
+if ! ssh ubuntu@"$server" "docker info" >/dev/null 2>&1; then
+    echo "Error: Unable to execute Docker commands with user ubuntu."
+    exit 1
+fi
