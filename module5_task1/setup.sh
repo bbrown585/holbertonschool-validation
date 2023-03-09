4aaf4bb77a038d719533a16b4751ea0892f5e449
Brandon <brandon_scott585@yahoo.com>
2023-03-09 15:43:32 -0600

    add go

--- module5_task1/setup.sh
+++ module5_task1/setup.sh
@@ -5,10 +5,10 @@ apt upgrade -y
 apt install -y wget make shellcheck zip
 
 # Download Go
- wget https://go.dev/dl/go1.20.1.linux-amd64.tar.gz
- tar -C /usr/local -xzf go1.20.1.linux-amd64.tar.gz
- rm ./go1.20.1.linux-amd64.tar.gz
- export PATH=$PATH:/usr/local/go/bin
+#  wget https://go.dev/dl/go1.20.1.linux-amd64.tar.gz
+#  tar -C /usr/local -xzf go1.20.1.linux-amd64.tar.gz
+#  rm ./go1.20.1.linux-amd64.tar.gz
+#  export PATH=$PATH:/usr/local/go/bin
 
 # Download the Hugo executable
 wget https://github.com/gohugoio/hugo/releases/download/v0.110.0/hugo_0.110.0_Linux-64bit.tar.gz
