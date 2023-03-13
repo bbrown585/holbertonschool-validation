99c9715c8f62b1f4754c45ae5ef2fd7094d016b8
Brandon <brandon_scott585@yahoo.com>
2023-03-13 10:13:24 -0500

    task update dockerfile

--- module5_task2/deploy.sh
+++ module5_task2/deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # Read the IP address from the file
-ip_address=$(cat ip_address.txt)
+new_ec2_instance_public_ip=$(cat new_ec2_instance_public_ip.txt)
 
 # Read the name of the tag to use with the docker image
 dockerImageTag=$(cat awesome_image_tag_name.txt)
 
-scp -o StrictHostKeyChecking=accept-new ./awesome.tar.zip ubuntu@"$ip_address":/home/ubuntu/
+docker context create production --docker "host=ssh://ubuntu@$new_ec2_instance_public_ip"
+docker context use production
 
-ssh -o StrictHostKeyChecking=accept-new ubuntu@"$ip_address" "
-	unzip ./awesome.tar.zip
-	docker load --input awesome.tar
-	docker run --detach -p 80:9999 -p 443:9999 --restart='always' awesome:$dockerImageTag
-"
+docker pull bbrown585/awesome-web:"$dockerImageTag"
+docker compose up --detach awesome-web
--- module5_task2/ensure-server-setup.sh
+++ module5_task2/ensure-server-setup.sh
@@ -6,6 +6,9 @@ ec2InstanceId=$(aws ec2 describe-instances --filters "Name=ip-address,Values=$1"
 # Wait on this ec2 instance to be ready before trying to SSH into it
 aws ec2 wait instance-running --instance-ids $ec2InstanceId
 
+# Add this server as a known host
+ssh-keyscan -H "$1" >> ~/.ssh/known_hosts
+
 # Send commands to the server
 # Update the apt registry: sudo apt update
 # Upgrade all installed software: sudo apt upgrade -y
@@ -13,7 +16,8 @@ aws ec2 wait instance-running --instance-ids $ec2InstanceId
 # Start Docker: sudo systemctl start docker
 # Make Docker start on boot of the system: sudo systemctl enable docker
 # Add the ubuntu user to the docker group - this will let us use ssh to communicate with docker from a client: sudo usermod -aG docker ubuntu
-ssh -o StrictHostKeyChecking=accept-new -o ConnectTimeout=60 ubuntu@"$1" "
+
+ssh ubuntu@"$1" "
 	sudo apt update
 	sudo apt upgrade -y
 	sudo apt install docker.io unzip -y
@@ -21,5 +25,6 @@ ssh -o StrictHostKeyChecking=accept-new -o ConnectTimeout=60 ubuntu@"$1" "
 	sudo systemctl enable docker
 	sudo usermod -aG docker ubuntu
 "
-# save the ip address
-echo "$1" > ip_address.txt
+
+# Save the EC2 instance IP address for other scripts to use
+echo "$1" > new_ec2_instance_public_ip.txt
